a92d9fd4ea5b7d141f8e96a1b9832889e2058536
ConnorMinielly <Connor_Minielly@Outlook.com>
2018-08-13 15:13:46 -0400

    added route for showing surveys

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,13 @@ var router = express.Router();
 // create a link to our users controller
 var usersController = require('../controllers/usersController');
 
+// Route to show user's surveys.
+router.get('/:id/mysurveys', usersController.mySurveys);
 // new (http://my-app.com/users/new)
-router.get( '/new', usersController.new );
+router.get('/new', usersController.new);
 
 // create (http://my-app.com/users)
-router.post( '/', usersController.create )
+router.post('/', usersController.create);
 
 // makes our file public to the application
 module.exports = router;
